05ecfab142c48701a455edf92197dbb071fea7ec
Dawid Mandyna <dawid.mandyna@gmail.com>
2024-09-01 15:00:07 +0100

    feat: add asteroid objects and use groups for drawable and updatable objects

--- main.py
+++ main.py
@@ -2,28 +2,44 @@ import pygame
 
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 
 def main():
     pygame.init()
-    print("Starting asteroids!")
-    print("Screen width:", SCREEN_WIDTH)
-    print("Screen height:", SCREEN_HEIGHT)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, drawable, updatable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+
+    Player.containers = (drawable, updatable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     dt = 0
     running = True
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        for obj in updatable:
+            obj.update(dt)
+
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+
+        for obj in drawable:
+            obj.draw(screen)
 
         pygame.display.flip()
+
         dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
